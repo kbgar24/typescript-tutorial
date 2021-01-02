b6a88a28c57d09642d99b391c088f301ec858456
Kendrick Gardner <kbgar24@gmail.com>
2021-01-02 12:44:38 -0600

    handle multiiple types

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -5,7 +5,7 @@ const coordinates: {x: number, y: number} = JSON.parse(json)
 
 console.log(coordinates)
 
-// 2) When we declare variable on noe line and initialize it later
+// 2) When we declare variable on one line and initialize it later
 let words = ['red', 'green', 'blue'];
 
 let foundWord = false;
@@ -18,7 +18,9 @@ for (let i = 0; i < words.length; i++) {
 
 //  3 Variable whose type cannot be inferred correctly
 let numbers = [-10, -1, 12];
-let numberAboveZero = false;
+
+// Use | for 'or' in type annotation
+let numberAboveZero: boolean | number = false;
 
 for (let i = 0; i < numbers.length; i++){
     if (numbers[i] > 0) {
