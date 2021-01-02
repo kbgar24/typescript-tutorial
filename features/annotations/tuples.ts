16be545016ec460657b35bb9db49334baf303ce7
Kendrick Gardner <kbgar24@gmail.com>
2021-01-02 13:19:24 -0600

    tuples v obojects

--- features/annotations/tuples.ts
+++ features/annotations/tuples.ts
@@ -9,3 +9,11 @@ type Drink = [string, boolean, number];
 //Re-write object as array of values
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 30];
+
+const carSpects: [number, number] = [400, 3354];
+
+//Objects may be more useful than tuples due to keys that are easy to read
+const carStats =  {
+    horsepower: 300,
+    weight: 3354
+};
